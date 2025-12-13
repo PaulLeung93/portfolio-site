7c30e5db5b621cb799bd0e83e5c13c56f4303755
PaulLeung93 <PaulLeung93@gmail.com>
2025-12-12 19:42:20 -0500

    Remove base path from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-site/', // Use relative paths for flexible deployment (works for both user and project pages)
 })
